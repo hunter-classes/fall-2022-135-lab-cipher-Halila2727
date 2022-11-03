36f4e23cdacaa6e79bc08b39a284afc4b85032c3
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-03 04:24:11 -0400

    added output for solve

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ int main()
   std::cout << encryptCaesar("This is a really difficult assignment in my opinion.", 10);
   std::cout << "\n---\n";
   
-  std::cout << "\ndecryptCaesar() function:\nciphertext: Drsc sc k bokvvi nsppsmevd kccsqxwoxd sx wi yzsxsyx. \nShift:\n";
-  std::cout << decryptCaesar("Drsc sc k bokvvi nsppsmevd kccsqxwoxd sx wi yzsxsyx.", 10);
+  std::cout << "\nsolve() function:\nciphertext: Drsc sc k bokvvi nsppsmevd kccsqxwoxd sx wi yzsxsyx.\n";
+  std::cout << solve("Drsc sc k bokvvi nsppsmevd kccsqxwoxd sx wi yzsxsyx.");
   std::cout << "\n------------------\n";
 
   std::cout << "\nencryptCaesar() function:\nplaintext: I really enjoy simulation-based sports games where you become a general manager. \nShift: 23\n";
   std::cout << encryptCaesar("I really enjoy simulation-based sports games where you become a general manager.", 23);
   std::cout << "\n---\n";
   
-  std::cout << "\ndecryptCaesar() function:\nciphertext: F obxiiv bkglv pfjrixqflk-yxpba pmloqp dxjbp tebob vlr ybzljb x dbkboxi jxkxdbo.\n";
-  std::cout << decryptCaesar("F obxiiv bkglv pfjrixqflk-yxpba pmloqp dxjbp tebob vlr ybzljb x dbkboxi jxkxdbo.", 23);
+  std::cout << "\nsolve() function:\nciphertext: F obxiiv bkglv pfjrixqflk-yxpba pmloqp dxjbp tebob vlr ybzljb x dbkboxi jxkxdbo.\n";
+  std::cout << solve("F obxiiv bkglv pfjrixqflk-yxpba pmloqp dxjbp tebob vlr ybzljb x dbkboxi jxkxdbo.");
   std::cout << "\n------------------\n";
   return 0;
 }
